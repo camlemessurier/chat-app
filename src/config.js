a90e119dcf5ef716d5bcf13a82c5b3a88de0d167
Cam Le Messurier <camlemessurier@gmail.com>
2020-09-24 18:54:23 +1000

    first deploy

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	apiKey: "AIzaSyDBlchIQk1uIQ_1ZhvUD45NO52FHlIL93c",
+	authDomain: "chat-app-8af6e.firebaseapp.com",
+	databaseURL: "https://chat-app-8af6e.firebaseio.com",
+	projectId: "chat-app-8af6e",
+	storageBucket: "chat-app-8af6e.appspot.com",
+	messagingSenderId: "333733003501",
+	appId: "1:333733003501:web:0f347b210655cdc6ea8ce4",
+	measurementId: "G-9TGS9WY5TG",
+};
